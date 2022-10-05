0b8e8475576c69c83794d7de654dbf9dffa4526f
laura-bluz <laura.bluz@hotmail.com>
2022-10-05 17:53:28 -0300

    Arrumando CSS e Posts da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
@@ -25,18 +27,29 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home(props: PostPagination): JSX.Element {
   return (
     <>
       <div className={styles.home}>
         <h1>Home</h1>
-        <button type="button">Carregar mais posts</button>
+        <Image src="/public/Logo.svg" alt="logo" width="100px" height="100px" />
+        {/* position: absolute;
+        width: 238.62px;
+        height: 25.63px;
+        left: 372px;
+        top: 79.55px; */}
+
+        {/* {
+          (props.next_page) ?
+            <button type="button">Carregar mais posts</button>
+        : props.next_page === false
+        } */}
       </div>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-// const prismic = getPrismicClient({});
-// const postsResponse = await prismic.getByType(TODO);
+// export const getStaticProps = async (): JSX.Element => {
+//   const prismic = getPrismicClient();
+//   const postsResponse = await prismic.getByType();
 // };
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,5 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +27,21 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>{post.data.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.data.title}</h1>
+        </article>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient({});

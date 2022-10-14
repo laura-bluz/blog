719faa759358d3869f0546d4b853c2d76cb953e0
laura-bluz <laura.bluz@hotmail.com>
2022-10-14 16:58:14 -0300

    conexão com Prismic feita corretamente

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,38 +1,35 @@
-// import * as Prismic from '@prismicio/client';
+// import * as prismic from '@prismicio/client';
+// import { HttpRequestLike } from '@prismicio/client';
+// import { enableAutoPreviews } from '@prismicio/next';
 // import { IncomingMessage } from 'http';
 
-// export function getPrismicClient(
-//   req?: IncomingMessage & {
-//     cookies: Partial<{
-//       [key: string]: string;
-//     }>;
-//   }
-// ): JSX.Element {
-//   const prismic = Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+// export interface PrismicConfig {
+//   req?: HttpRequestLike;
+// }
+// // export function getPrismicClient(req?: IncomingMessage & Config: PrismicConfig): prismic.Client {
+// export function getPrismicClient(req?: IncomingMessage): prismic.Client {
+//   const client = prismic.createClient(process.env.PRISMIC_ENDPOINT);
+
+//   enableAutoPreviews({
+//     client,
 //     ...req,
-//     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
 //   });
-//   return prismic;
+
+//   return client;
 // }
 
-import * as prismic from '@prismicio/client';
-import { HttpRequestLike } from '@prismicio/client';
-import { enableAutoPreviews } from '@prismicio/next';
+// import Prismic from '@prismicio/client';
+import * as Prismic from '@prismicio/client';
 import { IncomingMessage } from 'http';
+import DefaultClient from '@prismicio/client/';
 
-export interface PrismicConfig {
-  req?: HttpRequestLike;
-}
-// export function getPrismicClient(req?: IncomingMessage & Config: PrismicConfig): prismic.Client {
-export function getPrismicClient(req?: IncomingMessage): prismic.Client {
-  const client = prismic.createClient(process.env.PRISMIC_ENDPOINT);
-
-  enableAutoPreviews({
-    client,
+export function getPrismicClient(req?: IncomingMessage): Prismic.Client {
+  const prismic = Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
     ...req,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
   });
 
-  return client;
+  return prismic;
 }
 
 // export function getPrismicClient(req?: IncomingMessage, {
